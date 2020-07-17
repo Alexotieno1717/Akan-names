e4c69f5f68aa42810c5bb18702bd076ba9b17155
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-17 18:26:56 +0300

    commented all js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
-var century;
-var year;
-var month;
-var dayOfMonth;
-
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
-function submit() {
-  century = parseInt(document.getElementById("century").value);
-  year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("date").value);
 
 
-//   if (century === "") {
+// var century;
+// var year;
+// var month;
+// var dayOfMonth;
+
+// var daysOfWeek = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+// var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+// function submit() {
+//   event.preventDefault();
+//   century = parseInt(document.getElementById("century").value);
+//   year = parseInt(document.getElementById("year").value);
+//   month = parseInt(document.getElementById("month").value);
+//   dayOfMonth = parseInt(document.getElementById("date").value);
+//   error = document.getElementById("result");
+
+//   //Validate
+//   if (century === "" || century < 1 || century > 21) {
 //     alert("Input Correct century");
 //     return false;
-//   } else if (year === "") {
+//   } else if (year === "" || year < 1 || year > 2030) {
 //     alert("Input Correct century");
 //     return false;
-//   } else if (month === "") {
+//   } else if (month === "" || month < 1 || month > 12) {
 //     alert("Input Correct century");
-//   } else if (dayOfMonth === "") {
+//   } else if (dayOfMonth === "" || dayOfMonth < 1 || dayOfMonth > 31) {
+//     alert("Input Correct century");
+//   } else {
 //     return false;
 //   }
-}
+// }
+
+// // function checkResult() {
+// //     event.preventDefault();
+// //   let firstName = document.getElementById("firstName").value;
+// //   let lastName = document.getElementById("lastName").value;
+// //   let output = document.getElementById("result");
+// //   let error = document.getElementById("result");
+
+// //   if (firstName.length >=1 && lastName.length>=1){
+// //     let fullName = `Your full name is: ${firstName} ${lastName}`;
+// //     output.innerHTML = fullName;
+// //   }else{
+// //       error.innerHTML="Error: input fill!"
+// //   }
+
+// // }
