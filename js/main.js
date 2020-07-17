a055155731544891cc4fa7422ca5a23dc56b5fe5
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-17 15:31:22 +0300

    Added var daysOfWeek, maleNames, femaleNames

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-var CC;
-var YY;
-var MM;
-var DD;
+var century;
+var year;
+var month;
+var dayOfMonth;
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function submit() {
+  century = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  dayOfMonth = parseInt(document.getElementById("date").value);
+
+//   if (century === "") {
+//     alert("Input Correct century");
+//     return false;
+//   } else if (year === "") {
+//     alert("Input Correct century");
+//     return false;
+//   } else if (month === "") {
+//     alert("Input Correct century");
+//   } else if (dayOfMonth === "") {
+//     return false;
+//   }
+}
